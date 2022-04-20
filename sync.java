343f388e7be30aa0a4f2561726249ad33959535a
Ashok Kumar Bera <ashok.bera@zee.com>
2022-04-20 12:56:42 +0530

    first commit

---
+++ sync.java
@@ -0,0 +1,21 @@
+class sync
+{
+  public static void main(String args[])
+  {
+   int a[]= {10,20,30,40,50};
+   int b[] = {60,70,40,100};
+   for(int i=0;i<a.length;i++)
+   {
+    for(int j=0;j<b.length;j++)
+    {
+      if(a[i]==b[j])
+        {
+          System.out.println(a[i]);
+        }
+    }
+   }
+  }
+}
+
+
+   
